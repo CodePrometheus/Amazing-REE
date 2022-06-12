e4cb0165c7201ffaa9a79448263ebd4658b6b422
Starry <codeprince2020@163.com>
2022-06-12 17:27:08 +0800

    feat: captcha

--- elixir-learning/captcha/lib/captcha.ex
+++ elixir-learning/captcha/lib/captcha.ex
@@ -3,18 +3,24 @@ defmodule Captcha do
   Documentation for `Captcha`.
   """
   def new(phone, code) do
-    %{phone: phone, code: code}
+    %{phone: phone, code: code, remaining: 3}
   end
 
-  def verify(captcha, phone, code) do
-    if captcha.phone == phone and captcha.code == code do
-      {:ok, captcha}
-    else
-      if captcha.phone == phone do
-        {:error, captcha}
-      else
-        {:captcha_expired, captcha}
-      end
-    end
+  defp decrease_remaining(%{remaining: remaining} = captcha) do
+    %{captcha | remaining: remaining - 1}
+  end
+
+  def verify(%{phone: phone, code: code, remaining: remaining} = captcha, phone, code)
+      when remaining > 0 do
+    {:ok, decrease_remaining(captcha)}
+  end
+
+  def verify(%{phone: phone, remaining: remaining} = captcha, phone, _code)
+      when remaining > 0 do
+    {:error, decrease_remaining(captcha)}
+  end
+
+  def verify(captcha, _phone, _code) do
+    {:captcha_expired, captcha}
   end
 end
